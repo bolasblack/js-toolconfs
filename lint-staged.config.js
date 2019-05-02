940dec523b8ecc9efce7ffcfb20cd7eff1e89991
c4605 <bolasblack@gmail.com>
2019-05-02 22:54:39 +0800

    feat(lint-staged): move origin options under `linters` key

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,10 @@
 const commonProcess = ['yarn prettier --write', 'git add']
 
 module.exports = {
-  '*.{ts,tsx}': [...commonProcess],
-  '*.{js,jsx}': [...commonProcess],
-  '*.{css,scss,sass,less}': [...commonProcess],
-  '*.{md,mdx}': [...commonProcess],
+  linters: {
+    '*.{ts,tsx}': [...commonProcess],
+    '*.{js,jsx}': [...commonProcess],
+    '*.{css,scss,sass,less}': [...commonProcess],
+    '*.{md,mdx}': [...commonProcess],
+  },
 }
