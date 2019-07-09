9256ddcd8821242911becdbe1d15f1b8d4142e88
c4605 <bolasblack@gmail.com>
2019-07-09 11:33:42 +0800

    feat(lint-staged): upgrade to 9.x

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,16 @@
-const commonProcess = ['yarn prettier --write', 'git add']
+const commonProcess = filenames => {
+  const args = filenames.map(f => '"' + f + '"').join(' ')
+
+  // prettier-ignore
+  return [
+    'yarn prettier --write ' + args,
+    'git add ' + args,
+  ]
+}
 
 module.exports = {
-  linters: {
-    '*.{ts,tsx}': [...commonProcess],
-    '*.{js,jsx}': [...commonProcess],
-    '*.{css,scss,sass,less}': [...commonProcess],
-    '*.{md,mdx}': [...commonProcess],
-  },
+  '*.{ts,tsx}': commonProcess,
+  '*.{js,jsx}': commonProcess,
+  '*.{css,scss,sass,less}': commonProcess,
+  '*.{md,mdx}': commonProcess,
 }
