f3238d7faef60c668ea3ddd6cd30205d30043abb
c4605 <bolasblack@gmail.com>
2019-07-28 14:01:57 +0800

    feat(eslint): add eslint rule

--- eslintrc-ts.js
+++ eslintrc-ts.js
@@ -1,3 +1,5 @@
+const jsConf = require('./eslintrc-js')
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
@@ -18,10 +20,24 @@ module.exports = {
     '@typescript-eslint/no-parameter-properties': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     // warn
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-unused-vars': jsConf.rules['no-unused-vars'],
     '@typescript-eslint/explicit-member-accessibility': [
       'warn',
       { accessibility: 'no-public' },
     ],
-    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-object-literal-type-assertion': [
+      'warn',
+      {
+        allowAsParameter: true,
+      },
+    ],
+    // for next version of eslint
+    // '@typescript-eslint/consistent-type-assertions': [
+    //   'warn',
+    //   {
+    //     objectLiteralTypeAssertions: 'allow-as-parameter',
+    //   },
+    // ],
   },
 }
