8a5f2215fdeb658b354941dc93b8d74b27bfd196
c4605 <bolasblack@gmail.com>
2019-08-07 10:47:57 +0800

    fix(lint-staged): fix lint-staged configs not work

--- lint-staged.helpers.js
+++ lint-staged.helpers.js
@@ -1,14 +1,19 @@
 const wrap = fn => next => (filenames, commands) =>
-  next(filenames, (commands || []).concat(fn(filenames)))
+  next(filenames, commands || []).concat(fn(filenames))
 
-const finish = (filenames, commands) => []
+const tap = fn => next => (filenames, commands) => {
+  fn(filenames, commands)
+  return next(filenames, commands || [])
+}
+
+const finish = (filenames, commands) => commands
 
 const gitAdd = wrap(filenames => {
   const cliFileNames = wrapFileNames(filenames)
 
   // prettier-ignore
   return [
-    'yarn prettier --write ' + cliFileNames,
+    'git add ' + cliFileNames,
   ]
 })
 
@@ -17,7 +22,7 @@ const prettier = wrap(filenames => {
 
   // prettier-ignore
   return [
-    'git add ' + cliFileNames,
+    'yarn prettier --write ' + cliFileNames,
   ]
 })
 
@@ -32,9 +37,17 @@ const eslint = wrap(filenames => {
   ]
 })
 
+const prCmds = tap((filenames, cmds) => console.log('cmds', cmds))
+
 module.exports = {
-  wrap,
-  finish,
+  operators: {
+    wrap,
+    tap,
+    finish,
+  },
+  utils: {
+    prCmds,
+  },
   prettier,
   eslint,
   gitAdd,
